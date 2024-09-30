808eda61dba1a230d457ea064af3873bad9222df
sami.carret <sami.carret@epita.fr>
2024-09-30 14:05:31 +0200

    feat: domino-paving

--- dynamic_programming/domino_paving.py
+++ dynamic_programming/domino_paving.py
@@ -18,4 +18,17 @@ def domino_paving(n: int) -> int:
     """
     a = 0
     # BEGIN SOLUTION
+    if n % 2 != 0:
+        return 0
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    if n >= 1:
+        dp[1] = 0
+    if n >= 2:
+        dp[2] = 3
+    if n >= 3:
+        dp[3] = 0
+    for i in range(4, n + 1):
+        dp[i] = 4 * dp[i - 2] - dp[i - 4]
+    return dp[-1]
     # END SOLUTION
